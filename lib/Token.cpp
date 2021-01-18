3298aa4536884b91051f1e81207bd1bf78304be8
danielrrv <drodrigo678@gmail.com>
2021-01-17 23:01:12 -0500

    [Refactor] Initializer from implicit

--- lib/Token.cpp
+++ lib/Token.cpp
@@ -13,23 +13,10 @@ namespace IToken
 		std::string m_lexeme,m_literal;
 		int m_line;
 	public:
-		Token(TokenType type, std::string lexeme, std::string literal, int line)
-		{
-			m_type = type;
-			m_literal = literal;
-			m_lexeme = lexeme;
-			m_line = line;
-		}
-		std::string toString()
-		{
-			return m_lexeme;
-		}
-
-		std::string getLiteral(){
-			return m_literal;
-		}
-		std::string getLexeme() const{
-			return m_lexeme;
-		}
+		Token(TokenType type, std::string lexeme, std::string literal, int line):
+			m_type(type), m_lexeme(lexeme), m_literal(literal), m_line(line){}
+		std::string toString(void){return m_lexeme;}
+		std::string getLiteral(void){return m_literal;}
+		std::string getLexeme(void) const{return m_lexeme;}
 	};
 } // namespace IToken
